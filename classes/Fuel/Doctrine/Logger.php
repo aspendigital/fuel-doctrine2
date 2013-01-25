2116c44311324cc43f92cf659bc0b776f47b068f
Jim Cottrell <jim@aspendigital.com>
2013-01-24 17:29:15 -0700

    Replace Logger changes: rely on DBAL typing system to quote values; handle positional placeholders

--- classes/Fuel/Doctrine/Logger.php
+++ classes/Fuel/Doctrine/Logger.php
@@ -31,18 +31,13 @@ class Logger implements \Doctrine\DBAL\Logging\SQLLogger
 		{
 			// Attempt to replace placeholders so that we can log a final SQL query for profiler's EXPLAIN statement
 			// (this is not perfect-- getPlaceholderPositions has some flaws-- but it should generally work with ORM-generated queries)
-
+			
 			$is_positional = is_numeric(key($params));
-			if (is_null($types)) {
-				$types = array();
-				$params = array_values($params);
-				foreach ($params as $k => $p) {
-					$types[$k] = gettype($p);
-				}
-			}
 			list($sql, $params, $types) = \Doctrine\DBAL\SQLParserUtils::expandListParameters($sql, $params, $types);
+			if (empty($types))
+				$types = array();
 			$placeholders = \Doctrine\DBAL\SQLParserUtils::getPlaceholderPositions($sql, $is_positional);
-
+			
 			if ($is_positional)
 				$map = array_flip($placeholders);
 			else
@@ -54,32 +49,31 @@ class Logger implements \Doctrine\DBAL\Logging\SQLLogger
 						$map[$pos] = $name;
 				}
 			}
-
+			
 			ksort($map);
 			$src_pos = 0;
 			$final_sql = '';
+			$first_param_index = key($params);
 			foreach ($map as $pos=>$replace_name)
 			{
 				$final_sql .= substr($sql, $src_pos, $pos-$src_pos);
-				$src_pos = $pos + strlen($replace_name);
-
-				$current_param = $params[ltrim($replace_name, ':')];
-				$param_type = gettype($current_param);
-				if ($param_type == 'object') {
-					$param_class = get_class($current_param);
-					if ($param_class == 'DateTime') {
-						$current_param = $current_param->format('Y-m-d H:i:s');
-					} else {
-						$current_param = serialize($current_param);
-					};
-				} elseif ($param_type == 'array') {
-					$current_param = serialize($current_param);
+				
+				if ($sql[$pos] == ':')
+				{
+					$src_pos = $pos + strlen($replace_name);
+					$index = trim($replace_name, ':');
 				}
-				$final_sql .= \Fuel\Doctrine::manager()->getConnection()->quote($current_param);
+				else // '?' positional placeholder
+				{
+					$src_pos = $pos + 1;
+					$index = $replace_name + $first_param_index;
+				}
+				
+				$final_sql .= \Fuel\Doctrine::manager()->getConnection()->quote( $params[ $index ], \Arr::get($types, $index) );
 			}
-
+			
 			$final_sql .= substr($sql, $src_pos);
-
+			
 			$sql = $final_sql;
 		}
 
